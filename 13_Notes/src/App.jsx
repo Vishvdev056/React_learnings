cd12227d52074687fd77f776de2863f03217493f
Vishvdev Singh <vishvdevsingh056@gmail.com>
2026-09-20 15:46:58 +0530

    Add local storage project and update notes

--- 13_Notes/src/App.jsx
+++ 13_Notes/src/App.jsx
@@ -1,21 +1,46 @@
 import React from 'react'
 import { useState } from 'react'
+import { X } from 'lucide-react';
 
 const App = () => {
 const [title, setTitle] = useState('');
+const [details, setdetails] = useState('');
 
+const [task, settask] = useState([]);
 
   const submitHandeler=(elem)=>{
     elem.preventDefault()
-    console.log(title)
+
+const copyTask=[...task]
+
+copyTask.push({title,details})
+
+settask(copyTask)
+
+
+
+    // console.log(title)
+    // console.log(details)
 
     setTitle(' ')
+    setdetails(' ')
   }
+
+
+  const deleteNote=(idx)=>{
+    const copyTask=[...task]
+   
+
+    copyTask.splice(idx,1)
+
+    settask(copyTask)
+  }
+  
 
   
 
   return (
-    <div className='lg:flex  bg-black text-white   '>
+    <div className='w-full min-h-screen lg:flex  bg-black text-white   '>
       <form onSubmit={(elem)=>{
           submitHandeler(elem)
         }} className='flex  lg:w-1/2 items-start gap-5  flex-col p-10 '>
@@ -30,8 +55,19 @@ const [title, setTitle] = useState('');
            setTitle(elem.target.value)
            }}
            />
-        <textarea className=' px-5 py-2 w-full outline-none font-medium h-32 border-2 rounded' type="text" placeholder='Write Details' />
-        <button  className=' px-5 w-full  py-2 bg-blue-500 font-medium outline-none  bordder-1 rounded'>Add Notes</button>
+
+           {/* Dusra  input for Content  */}
+        <textarea className=' px-5 py-2 w-full outline-none font-medium h-32 border-2 rounded' type="text" placeholder='Write Details'
+        value={details}
+        onChange={(e)=>{
+          
+          setdetails(e.target.value)
+        }}
+         />
+
+          {/* button   */}
+
+        <button  className=' px-5 w-full active:bg-black  py-2 bg-blue-500 font-medium outline-none  bordder-1 rounded'>Add Notes</button>
   
       </form>
 
@@ -40,9 +76,19 @@ const [title, setTitle] = useState('');
   <h1  className='text-3xl font-bold'>Recent Notes</h1>
 
   <div className='   flex flex-wrap gap-5 mt-5 h-full overflow-auto'>
-        <div className=' w-40 h-52 bg-amber-100 rounded-2xl  '></div>
-        <div className=' w-40 h-52 bg-amber-100 rounded-2xl  '></div>
-        <div className=' w-40 h-52 bg-amber-100 rounded-2xl  '></div>
+       
+     {task.map(function(elem,idx){
+
+        return  <div key={idx} className=' relative w-40 h-52 p-4  rounded-2xl bg-cover  bg-[url("https://imgs.search.brave.com/Jbc-_lUBJ75B97FwxUOpotZdfer7axtMWIvTV64JhPk/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly9zdGF0/aWMudmVjdGVlenku/Y29tL3N5c3RlbS9y/ZXNvdXJjZXMvdGh1/bWJuYWlscy8wNjgv/NzcyLzU0OS9zbWFs/bC93YXRlcmNvbG9y/LXNwaXJhbC1ub3Rl/cGFkLWZyZWUtcG5n/LnBuZw")]'>
+
+          <h2 onClick={()=>{
+            deleteNote(idx)
+          }} className='absolute top-42 right-1 bg-red-500 text-xs p-1 rounded-full'  >  <X  /> </h2>
+          <h3 className='leading-tight text-xl mt-5 font-bold text-black'>{elem.title}</h3>
+          <p className=' mt-2 leading-tight font-medium text-gray-500  wrap-break-word'>{elem.details}</p>
+        </div>
+
+     })}
        
   </div>
 
